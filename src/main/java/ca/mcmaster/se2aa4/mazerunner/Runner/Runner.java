46dafed60a742e7d973718382d770ea975ebce58
Andy Pak <im.andy.pak@gmail.com>
2025-01-23 22:45:54 -0500

    Create runner class closing #10

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Runner/Runner.java
@@ -0,0 +1,29 @@
+package ca.mcmaster.se2aa4.mazerunner.Runner;
+
+import ca.mcmaster.se2aa4.mazerunner.Navigation.Direction;
+import ca.mcmaster.se2aa4.mazerunner.Navigation.Location;
+
+public class Runner {
+    private Location location;
+    private Direction direction;
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public Location getLocation() {
+        return this.location;
+    }
+
+    public Direction getDirection() {
+        return this.direction;
+    }
+
+    public void move() {
+        this.location = this.location.move(this.direction);
+    }
+}
