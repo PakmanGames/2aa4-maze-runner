402d03c93c7b93046b6c0e6fc974709c26c341ce
Andy Pak <im.andy.pak@gmail.com>
2025-01-23 20:47:27 -0500

    Create location class

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Navigation/Location.java
@@ -0,0 +1,27 @@
+package ca.mcmaster.se2aa4.mazerunner.Navigation;
+
+public class Location {
+    private int row;
+    private int column;
+
+    public Location(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getColumn() {
+        return this.column;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+}
